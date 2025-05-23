5cfdac6a985eb7f9b11e889fc2ea2484f03dbbf4
poojangit <poojang147@gmail.com>
2025-05-23 14:18:58 +0530

    [POOJA] Added : Added a method to find the maximum float value also added 3 test cases with max number at 1st, 2nd and 3rd

--- src/main/java/com/bridgelabz/testmaximum/FindMaximumValue.java
+++ src/main/java/com/bridgelabz/testmaximum/FindMaximumValue.java
@@ -2,11 +2,21 @@ package com.bridgelabz.testmaximum;
 
 public class FindMaximumValue {
    // Used Integer Object and compareTo method to test the maximum number
+    // Method to find the maximum Integer
     public static int findMaximumInteger(Integer a, Integer b, Integer c){
         Integer max=a;
         if(max.compareTo(b)<0)
             max=b;
         if(max.compareTo(c)<0)
+            max=c;
+        return max;
+    }
+   // Method to find the maximum float Value
+    public static Float findMaximumFloat(Float a, Float b, Float c){
+        Float max = a;
+        if(max.compareTo(b)<0.0f)
+            max=b;
+        if(max.compareTo(c)<0.0f)
             max=c;
         return max;
     }
