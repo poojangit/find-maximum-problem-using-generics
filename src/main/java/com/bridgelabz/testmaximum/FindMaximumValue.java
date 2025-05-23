5cadc00e3b93675e45cc82f0aca7a1e1364f065c
poojangit <poojang147@gmail.com>
2025-05-23 15:36:46 +0530

    [POOJA] Modified : Refactored all the 3 different methods to one generic method to find the maximum value

--- src/main/java/com/bridgelabz/testmaximum/FindMaximumValue.java
+++ src/main/java/com/bridgelabz/testmaximum/FindMaximumValue.java
@@ -3,29 +3,11 @@ package com.bridgelabz.testmaximum;
 public class FindMaximumValue {
    // Used Integer Object and compareTo method to test the maximum number
     // Method to find the maximum Integer
-    public static int findMaximumInteger(Integer a, Integer b, Integer c){
-        Integer max=a;
+    public static <E extends Comparable> E findMaximumValueGenericMethod(E a, E b, E c){
+        E max=a;
         if(max.compareTo(b)<0)
             max=b;
         if(max.compareTo(c)<0)
-            max=c;
-        return max;
-    }
-   // Method to find the maximum float Value
-    public static Float findMaximumFloat(Float a, Float b, Float c){
-        Float max = a;
-        if(max.compareTo(b)<0.0f)
-            max=b;
-        if(max.compareTo(c)<0.0f)
-            max=c;
-        return max;
-    }
-    //Method to find the maximum String value
-    public static String findMaximumString(String a, String b, String c){
-        String max = a;
-        if(max.compareTo(b) < 0)
-            max=b;
-        if(max.compareTo(c) < 0)
             max=c;
         return max;
     }
--- src/test/java/com/bridgelabz/testmaximum/FindMaximumValueTest.java
+++ src/test/java/com/bridgelabz/testmaximum/FindMaximumValueTest.java
@@ -9,28 +9,28 @@ class FindMaximumValueTest {
     //Test case for First maximum Integer value
     @Test
     public void findMaxIntegerAtFirstPos() {
-        int actualValue = FindMaximumValue.findMaximumInteger(100, 70, 30);
+        int actualValue = FindMaximumValue.findMaximumValueGenericMethod(100, 70, 30);
         int expectedValue = 100;
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
     //Test case for Second maximum Integer value
     @Test
     public void findMaxIntegerAtSecondPos() {
-        int actualValue = FindMaximumValue.findMaximumInteger(100, 700, 30);
+        int actualValue = FindMaximumValue.findMaximumValueGenericMethod(100, 700, 30);
         int expectedValue = 700;
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
     //Test case for Third maximum Integer value
     @Test
     public void findMaxIntegerAtThirdPos() {
-        int actualValue = FindMaximumValue.findMaximumInteger(100, 70, 300);
+        int actualValue = FindMaximumValue.findMaximumValueGenericMethod(100, 70, 300);
         int expectedValue = 300;
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
     //Test case for First maximum Float value
     @Test
     public void findMaxFloatAtFirstPos() {
-        Float actualValue = FindMaximumValue.findMaximumFloat(100.4f, 70.4f, 30.4f);
+        Float actualValue = FindMaximumValue.findMaximumValueGenericMethod(100.4f, 70.4f, 30.4f);
         Float expectedValue = 100.4f;
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
@@ -38,35 +38,35 @@ class FindMaximumValueTest {
     //Test case for Second maximum Float value
     @Test
     public void findMaxFloatAtSecondPos() {
-        Float actualValue = FindMaximumValue.findMaximumFloat(100.4f, 700.4f, 30.4f);
+        Float actualValue = FindMaximumValue.findMaximumValueGenericMethod(100.4f, 700.4f, 30.4f);
         Float expectedValue = 700.4f;
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
     //Test case for Third maximum Float value
     @Test
     public void findMaxFloatAtThirdPos() {
-        Float actualValue = FindMaximumValue.findMaximumFloat(100.4f, 70.4f, 300.4f);
+        Float actualValue = FindMaximumValue.findMaximumValueGenericMethod(100.4f, 70.4f, 300.4f);
         Float expectedValue = 300.4f;
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
     //Test case for First maximum String value at 1st position
     @Test
     public void findMaxStringAtFirstPos() {
-        String actualValue = FindMaximumValue.findMaximumString("Peach","Apple", "Banana");
+        String actualValue = FindMaximumValue.findMaximumValueGenericMethod("Peach","Apple", "Banana");
         String expectedValue = "Peach";
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
     //Test case for First maximum String value at 2nd position
     @Test
     public void findMaxStringAtSecondPos() {
-        String actualValue = FindMaximumValue.findMaximumString("Apple","Peach" ,"Banana");
+        String actualValue = FindMaximumValue.findMaximumValueGenericMethod("Apple","Peach" ,"Banana");
         String expectedValue = "Peach";
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
     //Test case for First maximum String value at 3rd position
     @Test
     public void findMaxStringAtThirdPos() {
-        String actualValue = FindMaximumValue.findMaximumString("Apple","Banana" ,"Peach");
+        String actualValue = FindMaximumValue.findMaximumValueGenericMethod("Apple","Banana" ,"Peach");
         String expectedValue = "Peach";
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
