428c1a8ac2bb45aa3fb3b8829ede6a66fe453018
poojangit <poojang147@gmail.com>
2025-05-23 15:06:37 +0530

    [POOJA] Added : Created a method to find the maximum String amoung the three Different Strings and checked with 3 test cases for all the 3 positions

--- src/main/java/com/bridgelabz/testmaximum/FindMaximumValue.java
+++ src/main/java/com/bridgelabz/testmaximum/FindMaximumValue.java
@@ -20,4 +20,13 @@ public class FindMaximumValue {
             max=c;
         return max;
     }
+    //Method to find the maximum String value
+    public static String findMaximumString(String a, String b, String c){
+        String max = a;
+        if(max.compareTo(b) < 0)
+            max=b;
+        if(max.compareTo(c) < 0)
+            max=c;
+        return max;
+    }
 }
--- src/test/java/com/bridgelabz/testmaximum/FindMaximumValueTest.java
+++ src/test/java/com/bridgelabz/testmaximum/FindMaximumValueTest.java
@@ -8,28 +8,28 @@ class FindMaximumValueTest {
 
     //Test case for First maximum Integer value
     @Test
-    public void findFirstMaximumInteger() {
+    public void findMaxIntegerAtFirstPos() {
         int actualValue = FindMaximumValue.findMaximumInteger(100, 70, 30);
         int expectedValue = 100;
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
     //Test case for Second maximum Integer value
     @Test
-    public void findSecondMaximumInteger() {
+    public void findMaxIntegerAtSecondPos() {
         int actualValue = FindMaximumValue.findMaximumInteger(100, 700, 30);
         int expectedValue = 700;
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
     //Test case for Third maximum Integer value
     @Test
-    public void findThirdMaximumInteger() {
+    public void findMaxIntegerAtThirdPos() {
         int actualValue = FindMaximumValue.findMaximumInteger(100, 70, 300);
         int expectedValue = 300;
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
     //Test case for First maximum Float value
     @Test
-    public void findFirstMaximumFloat() {
+    public void findMaxFloatAtFirstPos() {
         Float actualValue = FindMaximumValue.findMaximumFloat(100.4f, 70.4f, 30.4f);
         Float expectedValue = 100.4f;
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
@@ -37,16 +37,37 @@ class FindMaximumValueTest {
 
     //Test case for Second maximum Float value
     @Test
-    public void findSecondMaximumFloat() {
+    public void findMaxFloatAtSecondPos() {
         Float actualValue = FindMaximumValue.findMaximumFloat(100.4f, 700.4f, 30.4f);
         Float expectedValue = 700.4f;
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
     //Test case for Third maximum Float value
     @Test
-    public void findThirdMaximumFloat() {
+    public void findMaxFloatAtThirdPos() {
         Float actualValue = FindMaximumValue.findMaximumFloat(100.4f, 70.4f, 300.4f);
         Float expectedValue = 300.4f;
+        assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
+    }
+    //Test case for First maximum String value at 1st position
+    @Test
+    public void findMaxStringAtFirstPos() {
+        String actualValue = FindMaximumValue.findMaximumString("Peach","Apple", "Banana");
+        String expectedValue = "Peach";
+        assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
+    }
+    //Test case for First maximum String value at 2nd position
+    @Test
+    public void findMaxStringAtSecondPos() {
+        String actualValue = FindMaximumValue.findMaximumString("Apple","Peach" ,"Banana");
+        String expectedValue = "Peach";
+        assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
+    }
+    //Test case for First maximum String value at 3rd position
+    @Test
+    public void findMaxStringAtThirdPos() {
+        String actualValue = FindMaximumValue.findMaximumString("Apple","Banana" ,"Peach");
+        String expectedValue = "Peach";
         assertEquals(expectedValue, actualValue, "The maximum value is not correct! The Expected Value is " + expectedValue );
     }
 
